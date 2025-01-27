0265645eed77ae35209254ad80975422be6a4bae
DennisT <tabaldodennis@gmail.com>
2025-01-27 23:47:01 +0800

    Update tailwind.config.js
    sidebar config

--- tailwind.config.js
+++ tailwind.config.js
@@ -83,6 +83,18 @@ module.exports = {
                     from: { height: "var(--radix-collapsible-content-height)" },
                     to: { height: 0 },
                 },
+                sidebar: {
+                    DEFAULT: "hsl(var(--sidebar-background))",
+                    foreground: "hsl(var(--sidebar-foreground))",
+                    primary: "hsl(var(--sidebar-primary))",
+                    "primary-foreground":
+                        "hsl(var(--sidebar-primary-foreground))",
+                    accent: "hsl(var(--sidebar-accent))",
+                    "accent-foreground":
+                        "hsl(var(--sidebar-accent-foreground))",
+                    border: "hsl(var(--sidebar-border))",
+                    ring: "hsl(var(--sidebar-ring))",
+                },
             },
             animation: {
                 "accordion-down": "accordion-down 0.2s ease-out",
